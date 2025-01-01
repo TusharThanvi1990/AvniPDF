3a327fa90d0a9114f9d7f1ca785e7d01506876fb
Tushar Thanvi <143414158+TusharThanvi1990@users.noreply.github.com>
2025-01-01 13:33:58 +0530

    Update page.tsx

--- src/app/image-to-pdf/page.tsx
+++ src/app/image-to-pdf/page.tsx
@@ -3,15 +3,9 @@
 import dynamic from 'next/dynamic';
 import ImageToText from '../../../components/ImageToText';
 
-// Dynamically import the PDFTronEditor component with SSR disabled
-const ComingSoon = dynamic(() => import('../../../components/Comingsoon'), { 
-  ssr: false 
-});
-
 export default function Home() {
 
   return (
     <ImageToText />
-    // <ComingSoon />
   )
 }
