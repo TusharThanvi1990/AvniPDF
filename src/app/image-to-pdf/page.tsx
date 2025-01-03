7620aa19a201ad249cfdc767ff2a7abf75ab0dd0
Tushar Thanvi <tushar.thanvi2005@gmail.com>
2025-01-03 18:53:34 +0530

    Dynamically imported the component in the ImageToPdf to make it render on the cliend side to solve the buid error

--- src/app/image-to-pdf/page.tsx
+++ src/app/image-to-pdf/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-import ImageToText from '../../../components/ImageToText';
+import dynamic from 'next/dynamic';
+
+// Dynamically import the ImageToText component with client-side rendering
+const ImageToText = dynamic(() => import('../../../components/ImageToText'), { 
+  ssr: false 
+});
 
 export default function Home() {
-
   return (
     <ImageToText />
-  )
+  );
 }
